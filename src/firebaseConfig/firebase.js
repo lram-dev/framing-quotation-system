94d33fd0d8cc4f2d92065b47b30f0ebe223f8ec5
Ricardo Acosta <ricardo.acosta.meza.lram@gmail.com>
2026-03-13 01:43:41 -0600

    `Removed unused files and updated dependencies`

---
+++ src/firebaseConfig/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+
+import { getFirestore } from "@firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBB5VzUXe5sf-j-tFtJg-LYvSPFKvKQF9A",
+  authDomain: "framing-quotation-system.firebaseapp.com",
+  projectId: "framing-quotation-system",
+  storageBucket: "framing-quotation-system.firebasestorage.app",
+  messagingSenderId: "772482400021",
+  appId: "1:772482400021:web:16bb34c8ff32a068a3f31c",
+  measurementId: "G-KXWSY95D53",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
